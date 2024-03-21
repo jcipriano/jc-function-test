955214e969b5c33175458d2e3dd2b727ad529f2c
jcipriano <>
2024-03-21 10:45:02 -0700

    proxy request to B through A

--- netlify/functions/a.ts
+++ netlify/functions/a.ts
@@ -1,5 +1,16 @@
 import type { Context } from "@netlify/functions"
 
 export default async (req: Request, context: Context) => {
-  return new Response("A: Hello, world!")
+
+    // call function B
+    const response = await fetch(Netlify.env.get("URL") + '/.netlify/functions/b')
+
+    // return response from function B
+    return new Response(response.body, {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+    });
+
 }
--- netlify/functions/b.ts
+++ netlify/functions/b.ts
@@ -1,5 +1,15 @@
 import type { Context } from "@netlify/functions"
 
 export default async (req: Request, context: Context) => {
-  return new Response("B: Hello, world!")
+
+  const response = JSON.stringify({
+    message: "Hello from function B!"
+  })
+
+  return new Response(response, {
+    status: 200,
+		headers: {
+			'Content-Type': 'application/json; charset=utf-8'
+		}
+  });
 }
